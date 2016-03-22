0cf5996a47fb88d22e219eb8ba00f7e3ee3217a9
Thirafi Dide <jurnaldide@gmail.com>
2016-03-22 22:21:10 +0700

    add karma config

---
+++ karma.conf.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack');
+
+module.exports = (config) => {
+  config.set({
+
+    browsers: [process.env.CI ? 'PhantomJS' : 'Chrome'],
+
+    singleRun: true,
+
+    frameworks: ['mocha'],
+
+    files: [
+      './node_modules/phantomjs-polyfill/bind-polyfill.js',
+      'tests.webpack.js',
+    ],
+
+    preprocessors: {
+      'tests.webpack.js': ['webpack', 'sourcemap'],
+    },
+
+    reporters: ['mocha'],
+
+    plugins: [
+      require('karma-webpack'),
+      require('karma-mocha'),
+      require('karma-mocha-reporter'),
+      require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
+      require('karma-sourcemap-loader'),
+    ],
+
+    webpack: {
+      devtool: 'inline-source-map',
+      module: {
+        loaders: [
+          { test: /\.(jpe?g|png|gif|svg)$/, loader: 'url', query: { limit: 10240 } },
+          { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel'] },
+          { test: /\.json$/, loader: 'json-loader' },
+          { test: /\.css$/, loader: 'style!css' },
+        ],
+      },
+      resolve: {
+        modulesDirectories: [
+          'app',
+          'node_modules',
+        ],
+        extensions: ['', '.json', '.js', '.jsx'],
+      },
+      plugins: [
+        new webpack.IgnorePlugin(/\.json$/),
+        new webpack.NoErrorsPlugin(),
+      ],
+    },
+
+    webpackServer: {
+      noInfo: true,
+    },
+
+  });
+};

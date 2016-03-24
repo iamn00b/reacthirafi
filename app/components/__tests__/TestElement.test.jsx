22fff84ee07f0f47fa53633c08f3ad980cb4232c
Thirafi Dide <jurnaldide@gmail.com>
2016-03-24 15:25:08 +0700

    use shallow renderer for stateless components

--- app/components/__tests__/TestElement.test.jsx
+++ app/components/__tests__/TestElement.test.jsx
@@ -5,8 +5,12 @@ import expect from 'expect';
 import TestElement from '../TestElement.jsx';
 
 describe('Test Component', () => {
+  const shallowRenderer = TestUtils.createRenderer();
+  shallowRenderer.render(<TestElement />);
+  const renderResult = shallowRenderer.getRenderOutput();
+
   it('renders without problems', () => {
-    const testElementComponent = TestUtils.renderIntoDocument(<TestElement />);
-    expect(testElementComponent).toExist();
+    expect(renderResult).toExist();
+    expect(renderResult.type).toBe('h1');
   });
 });
